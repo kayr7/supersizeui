7f5c6898470a4cd818a0af90507d5b03d2a85dd8
Oryak <Oryak@kay-rottmann.de>
2020-07-10 22:32:57 +0200

    added coverage test

--- tests/context.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from supersize import screengrab
-from supersize.screengrab import ScreenGrabber
-import supersize
